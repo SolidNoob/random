f6cfb63b4b0752f4ffa095d7688a7b483cffc5bf
SolidNoob <anthony.cammarata@hotmail.com>
2019-08-22 17:11:26 +0200

    use monolog

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+require 'vendor/autoload.php';
+
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('test');
+$log->pushHandler(new StreamHandler('mylog.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+
 echo 'Hello World<br/>';
 
 include 'feature01.php';
